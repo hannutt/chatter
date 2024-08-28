813a21c1caae79d29f1d4da3af171757dc150481
hannutt <htuomela@gmail.com>
2024-08-28 15:25:27 +0300

    First publish

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var users=0
+//tarvitaan, että ulkoinen css-tiedosto saadaan käyttöön
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg,user,time,users) => {
+        //Tässä tapauksessa lähetämme viestin yksinkertaisuuden vuoksi kaikille, myös lähettäjälle.
+      io.emit('chat message', msg,user,time,users);
+
+      //tämä lähettää viestin siten, että se näkyy vain vastaanottajan ikkunassa
+      //socket.broadcast.emit('chat message',msg,user,time,users);
+    });
+  });
+  
+
+  //kun localhost:3000 avataan suoritetaan connect + lisätään aktiivinen käyttäjä
+io.on('connection', (socket) => {
+  users++
+  io.emit('count',users)
+    
+    console.log('a user connected', users);
+    
+    
+   
+  
+    
+
+    //jos localhost:3000 suljetaan, niin suoritetaan disconnect + vähennetään aktiivinen
+    socket.on('disconnect', () => {
+      users--
+      io.emit("count",users)
+      console.log('user disconnected',users);
+    });
+  });
+
+  
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
